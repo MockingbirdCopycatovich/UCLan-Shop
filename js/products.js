f06c4b21a65e5331adabd9cea6a67ecd6dabbdc6
Vladislav Vasilev <vvasilev1@uclan.ac.uk>
2025-12-14 17:24:45 +0300

    feat: Added ‘Back to top’ button

--- js/products.js
+++ js/products.js
@@ -116,4 +116,21 @@ document.addEventListener("click", (event) => {
     }
 });
 
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 150) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
 renderProducts();
